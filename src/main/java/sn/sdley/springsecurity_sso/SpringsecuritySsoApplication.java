2ef1a0e5e73d076072445c4b38a08b7a1aa64977
Souleymane DIALLO <sdley2020@gmail.com>
2026-01-17 13:50:01 +0000

    Initial commit

---
+++ src/main/java/sn/sdley/springsecurity_sso/SpringsecuritySsoApplication.java
@@ -0,0 +1,13 @@
+package sn.sdley.springsecurity_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritySsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritySsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/sdley/springsecurity_sso/SpringsecuritySsoApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.sdley.springsecurity_sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritySsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
